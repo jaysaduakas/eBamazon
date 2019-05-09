351053c9cc4ba0b193554046670d80e87192ad5e
MarissaAlmoslino <marissaalmoslino@gmail.com>
2019-05-09 19:53:16 -0400

    Added Classes: Auction, AuctionImage, AuctionKeyword, and Bid

--- src/Ebamazon/model/User.java
+++ src/Ebamazon/model/User.java
@@ -25,7 +25,6 @@ public class User {
             Connection con = DBConnection.getConnection();
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(query);
-
             if (rs.next()){
                 truthFlag=true;
             }
